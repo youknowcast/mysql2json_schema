bb3c632d0883dc91910ba50e22e5c6ff1fe9191b
AMBE Hideki <ambe.hideki@bell-face.com>
2021-02-02 12:24:39 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import * as fs from 'fs'
+import mysqlspec = require('mysqlspec')
+
+require('dotenv').config()
+const env = process.env
+
+// Mysql connect config.
+var config = {
+    user: env.MYSQL_USER,
+    password: env.MYSQL_PASSWD,
+    host: env.MYSQL_HOST,
+    database: env.MYSQL_DATABASE
+};
+const prefix = env.SCHEMA_PREFIX
+const filename = prefix !== '' ? `${prefix}_schema.json` : 'schema.json'
+
+function callback(err: Error, schema) {
+  try {
+    fs.writeFileSync('staffs_schema.json', JSON.stringify(schema, null, 2))
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const table = env.MYSQL_TABLE
+if (table !== '') {
+  mysqlspec(config, env.MYSQL_DATABASE, table, callback)
+} else {
+  mysqlspec(config, env.MYSQL_DATABASE, callback)
+}
